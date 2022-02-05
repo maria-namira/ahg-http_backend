94ca94578828e067e7b85e365b41576a5ee67933
maria-namira <63401949+maria-namira@users.noreply.github.com>
2022-02-05 11:38:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ exports.start = async () => {
     http.createServer(app.callback()).listen(port);
     console.log(`The server is running on port ${port}`);
     console.log('http://localhost:3000')
-    console.log('https://ahj-http-image-manager.herokuapp.com/');
+    console.log('https://coursar.herokuapp.com/ deployed to Heroku/');
   }
   catch (err) {
     console.log(err);
